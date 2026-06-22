1fb085819b77e130466ca80b9ff4e40cf9d8afac
Son Do <dominhson580@gmail.com>
2026-06-22 23:09:53 +0700

    [improve] efficient buffer operations

--- src/common.cpp
+++ src/common.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 // system
 #include <unistd.h>
 
@@ -72,11 +73,59 @@ int32_t writen(int fd, const uint8_t *buf, size_t n)
 /* append len bytes to back of buf. */
 void buf_append(Buffer &buf, const uint8_t *data, size_t len)
 {
-    buf.insert(buf.end(), data, data + len);
+    if (len == 0)
+        return;
+
+    size_t free_space_at_back = buf.end - buf.data_end;
+
+    if (len > free_space_at_back)
+    {
+        size_t data_size = buf.data_end - buf.data_begin;
+        size_t total_capacity = buf.end - buf.begin;
+        size_t total_free_space = total_capacity - data_size;
+
+        if (len <= total_free_space)
+        { // enough space but fragmented -> shift data to front
+            memmove(buf.begin, buf.data_begin, data_size);
+            buf.data_begin = buf.begin;
+            buf.data_end = buf.begin + data_size;
+        }
+        else
+        { // not enough space even when shifting
+            size_t new_capacity =
+                (total_capacity == 0) ? 64 : total_capacity * 2;
+            while (new_capacity < data_size + len)
+                new_capacity *= 2;
+
+            size_t free_space_at_front = buf.data_begin - buf.begin;
+            buf.begin = (uint8_t *)realloc(buf.begin, new_capacity);
+            assert(buf.begin != NULL);
+
+            // re-align pointers
+            buf.data_begin = buf.begin + free_space_at_front;
+            buf.data_end = buf.begin + free_space_at_front + data_size;
+            buf.end = buf.begin + new_capacity;
+        }
+    }
+
+    // copy data and advance buf.data_end pointer
+    if (data != NULL)
+        memcpy(buf.data_end, data, len);
+    buf.data_end += len;
 }
 
 /* remove n bytes from front of buf. */
 void buf_consume(Buffer &buf, size_t n)
 {
-    buf.erase(buf.begin(), buf.begin() + n);
+    size_t data_size = buf.data_end - buf.data_begin;
+    assert(n <= data_size);
+
+    if (n == data_size)
+    { // reset both data pointers to buf.begin (maximize write space)
+        buf.data_begin = buf.data_end = buf.begin;
+    }
+    else
+    { // only advance buf.data_begin pointer, don't need to move data
+        buf.data_begin += n;
+    }
 }
--- src/common.hpp
+++ src/common.hpp
@@ -3,6 +3,7 @@
 // stdlib
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 // C++
 #include <vector>
 
@@ -39,6 +40,56 @@ void die(const char *msg);
 int32_t readn(int fd, uint8_t *buf, size_t n);
 int32_t writen(int fd, const uint8_t *buf, size_t n);
 
-typedef std::vector<uint8_t> Buffer;
+struct Buffer;
 void buf_append(Buffer &buf, const uint8_t *data, size_t len);
 void buf_consume(Buffer &buf, size_t n);
+
+typedef struct Buffer
+{
+  uint8_t *begin = NULL;
+  uint8_t *end = NULL; // exclusive
+  uint8_t *data_begin = NULL;
+  uint8_t *data_end = NULL; // exclusive
+
+  inline size_t size() const
+  {
+    return data_end - data_begin;
+  }
+
+  inline uint8_t *data()
+  {
+    return data_begin;
+  }
+  inline const uint8_t *data() const
+  {
+    return data_begin;
+  }
+
+  inline uint8_t &operator[](size_t index)
+  {
+    return data_begin[index];
+  }
+  inline const uint8_t &operator[](size_t index) const
+  {
+    return data_begin[index];
+  }
+
+  inline void resize(size_t new_data_size)
+  {
+    size_t data_size = data_end - data_begin;
+    if (new_data_size == data_size) // unchanged
+      return;
+
+    if (new_data_size < data_size)
+    { // truncate
+      data_end = data_begin + new_data_size;
+      return;
+    }
+
+    // grow by appending zero-initialized bytes
+    size_t growth_needed = new_data_size - data_size;
+    size_t write_offset = data_end - begin;
+    buf_append(*this, NULL, growth_needed); // might reallocate and shift 'begin'
+    memset(begin + write_offset, 0, growth_needed);
+  }
+} Buffer;
